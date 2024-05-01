9bf8e88ae6c8d60011f0b7948d4a6dbfd90ccb00
maiyerhn <118492521+maiyerhn@users.noreply.github.com>
2024-05-01 13:05:59 -0500

    Navigator sin test

---
+++ Css-JS/Scripts.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    let lon;
+    let lat;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(posicion => {
+            lon = posicion.coords.longitude
+            lat = posicion.coords.latitude
+
+            const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={66b40fc04c4573ca11f70b421f5e367e}`;
+            console.log(url);
+        })
+    }
+});
