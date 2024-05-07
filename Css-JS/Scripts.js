78d76d8dc95fe8318ac399c77a559b961fdc3378
maiyerhn <118492521+maiyerhn@users.noreply.github.com>
2024-05-07 16:27:29 -0500

    Geo testeada

--- Css-JS/Scripts.js
+++ Css-JS/Scripts.js
@@ -7,7 +7,9 @@ window.addEventListener("load", () => {
             lat = posicion.coords.latitude
 
             const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid={66b40fc04c4573ca11f70b421f5e367e}`;
-            console.log(url);
+            fetch(url)
+            .then{ response => {return response.json() }}
+            .then{data => ()}
         })
     }
 });
